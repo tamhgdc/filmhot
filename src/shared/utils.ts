5d8af536ee387bc33232bc96f7e5aacd41daa2a9
tamhgdc <108126724+tamhgdc@users.noreply.github.com>
2022-06-30 13:35:02 +0700

    Add files via upload

---
+++ src/shared/utils.ts
@@ -0,0 +1,26 @@
+export const htmlToText = (html: string) => {
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  return div.textContent as string;
+};
+
+export const calculateCreatedTime = (timeCreated: number) => {
+  let periods = {
+    year: 365 * 30 * 24 * 60 * 60 * 1000,
+    month: 30 * 24 * 60 * 60 * 1000,
+    week: 7 * 24 * 60 * 60 * 1000,
+    day: 24 * 60 * 60 * 1000,
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+  };
+  let diff = Date.now() - timeCreated;
+
+  for (const key in periods) {
+    if (diff >= periods[key]) {
+      let result = Math.floor(diff / periods[key]);
+      return `${result} ${result === 1 ? key : key + "s"} ago`;
+    }
+  }
+
+  return "Just now";
+};
